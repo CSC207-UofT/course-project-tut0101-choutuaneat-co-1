a68ca984f7e37f9e85b0e8826fb66272bee93a23
KevenQiao <qiao_kevin@outlook.com>
2021-10-12 13:54:17 -0400

    ingredients updates

--- src/main/java/Ingredients.java
+++ src/main/java/Ingredients.java
@@ -1,2 +1,18 @@
 public class Ingredients {
+    String ingredients_name;
+    double calories;
+
+    public Ingredients (String ingredients_name, double calories){
+        this.ingredients_name = ingredients_name;
+        this.calories = calories;
+    }
+
+    public String getIngredientsName() {
+        return this.ingredients_name;
+    }
+
+    public double getCalories(){
+        return this.calories;
+    }
 }
+
