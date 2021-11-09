eef44ea457b30028c74e32992418f3a2736c4181
Kevenqqq <90425570+Kevenqqq@users.noreply.github.com>
2021-11-09 15:54:04 -0500

    Delete src directory

--- src/main/java/ChouTuanMaster.java
+++
@@ -1,9 +0,0 @@
-public class ChouTuanMaster {
-    public static void main(String[] args) {
-        cmdInterface newCMDInterface = new cmdInterface();
-        System.out.print("Welcome to ChouTuan Eat Application! Enjoy it!\n");
-        newCMDInterface.execute();
-        System.out.print("Thanks for using!\n");
-        return;
-    }
-}
