b472d309753c06ebed1dad44e43b041d9e63414a
LAPTOP-EIB7T5DG\Lenovo <1284673529@qq.com>
2021-11-12 10:14:57 -0500

    debug for frontend

--- src/main/java/ChouTuanEat.Co/controller/LoginController.java
+++ src/main/java/ChouTuanEat.Co/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("login")
 public class LoginController {
     private static User DUMMY_FORM_PLACEHOLDER_STUDENT = new User();
